910e5d3c54c8853d1ed8926f95550172ba78972d
XEZKalvin <xezkalvin@gmail.com>
2013-03-06 20:45:26 +0100

    same

--- xelitez/ironpp/Version.java
+++ xelitez/ironpp/Version.java
@@ -1,5 +1,5 @@
     public static int majorVersion = 3;
     public static int minorVersion = 3;
     public static int majorBuild = 4;
-    public static int minorBuild = 12;
+    public static int minorBuild = 13;
     public static String MC = "MC:1.4.7";
